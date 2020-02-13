b795c90f42cfa16b4ce8c7d63635795aa16821c8
Qoolqas <qoolqas@gmail.com>
2020-02-13 23:27:41 +0700

    refractor some junk

--- app/src/main/java/com/qoolqas/moviecatalogue/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/qoolqas/moviecatalogue/ui/tools/ToolsFragment.java
@@ -16,20 +16,10 @@ import com.qoolqas.moviecatalogue.R;
 
 public class ToolsFragment extends Fragment {
 
-    private ToolsViewModel toolsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        toolsViewModel =
-                ViewModelProviders.of(this).get(ToolsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        toolsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
